969749e15edbdb26b22a33aa135c3b399815209b
eric82714 <0958820714ab@gmail.com>
2019-12-04 17:04:20 +0800

    Writed by C++ language

---
+++ c++_version.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector index(2,0);
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; (j<nums.size() && j != i); j++){
+                if(nums[i] + nums[j] == target){
+                    index[0] = i;
+                    index[1] = j;
+                }
+            }
+        }
+        return index;
+    }
+};
